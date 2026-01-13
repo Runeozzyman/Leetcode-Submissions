df0683c6638f83d4e482fd36ac70d51284520478
Austin Wort <119460845+Runeozzyman@users.noreply.github.com>
2026-01-13 13:32:24 -0700

    Create Remove_Element(27).py

---
+++ Remove_Element(27).py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        
+        k = 0
+
+        for num in nums:
+            if num != val:
+                nums[k] = num
+                k += 1
+
+        return k
