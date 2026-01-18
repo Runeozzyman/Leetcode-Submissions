c340c72995dd703e013cb4cf76359391eecde293
Austin Wort <119460845+Runeozzyman@users.noreply.github.com>
2026-01-18 00:40:34 -0700

    Create Linked_List_Cycle(141).py

---
+++ Linked_List_Cycle(141).py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def hasCycle(self, head):
+       
+        if head is None:
+            return False
+
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if(slow == fast):
+                return True
+            
+        return False
