c03e5b435bc4b31f125d360bde588ed47a4b6947
Austin Wort <119460845+Runeozzyman@users.noreply.github.com>
2026-01-22 15:39:02 -0700

    Create Contains_Duplicate(217).py

---
+++ Contains_Duplicate(217).py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        
+        seen = {}
+
+        for num in nums:
+            if num in seen:
+                return True
+            seen[num] = 1
+
+        return False
