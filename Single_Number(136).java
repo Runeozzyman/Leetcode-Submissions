29a7acb0fa027a7851f2ae0f72ee5459b1124705
Austin Wort <119460845+Runeozzyman@users.noreply.github.com>
2026-02-06 20:32:03 -0700

    Create Single_Number(136).java

---
+++ Single_Number(136).java
@@ -0,0 +1,13 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        
+        int uniqueNum = 0;
+
+        for(int i=0; i<nums.length; i++){
+            uniqueNum ^= nums[i];
+        }
+
+        return uniqueNum;
+
+    }
+}
