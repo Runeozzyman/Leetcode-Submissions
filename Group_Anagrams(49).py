7101ce2964da15f33d08cd7be1625957bda54317
Austin Wort <119460845+Runeozzyman@users.noreply.github.com>
2026-08-20 16:12:43 -0600

    Create Group_Anagrams(49).py

---
+++ Group_Anagrams(49).py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        anagram_dict = defaultdict(list)
+
+        for s in strs:
+            count = [0] * 26 
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+
+            key = tuple(count)
+            anagram_dict[key].append(s)
+        
+        return anagram_dict.values()
+        
