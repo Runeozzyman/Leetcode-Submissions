936e72b4c3d6ff8b54c36e89ba083f55cd8ef0bf
Austin Wort <119460845+Runeozzyman@users.noreply.github.com>
2026-08-27 15:50:07 -0600

    Create Binary_Tree_Right_Side-View(199).py

---
+++ Binary Tree BFS/Binary_Tree_Right_Side-View(199).py
@@ -0,0 +1,26 @@
+
+
+#NOTE: This problem is intended to be solved with BFS but I find the DFS solution is much simpler and more elegant
+
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+
+        res = []
+
+        def dfs(node, depth):
+            if not node:
+                return
+            
+            if depth == len(res):
+                res.append(node.val)
+
+            dfs(node.right, depth+1)
+            dfs(node.left, depth+1)
+        
+        dfs(root,0)
+
+        return res 
