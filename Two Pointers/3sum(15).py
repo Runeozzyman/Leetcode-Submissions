7083bfb1fb4e3ba84e09d1602a15f358233a0570
Austin Wort <119460845+Runeozzyman@users.noreply.github.com>
2026-09-10 00:08:59 -0600

    Update 3sum(15).py

--- Two Pointers/3sum(15).py
+++ Two Pointers/3sum(15).py
@@ -1,38 +1,28 @@
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
-        
-        n = len(nums)
-        ans = []
-        sorted_nums = sorted(nums)
+        res = []
+        nums.sort()
 
-        for fixed in range(n - 2):
-
-            if fixed > 0 and sorted_nums[fixed] == sorted_nums[fixed - 1]:
+        for i in range(len(nums)):
+            if i>0 and nums[i] == nums[i-1]:
                 continue
+            
+            total = 0
+            j = i+1
+            k = len(nums) - 1
 
-            left = fixed + 1
-            right = n - 1
-            target = sorted_nums[fixed]
+            while j < k:
+                total = nums[i] + nums[j] + nums[k]
 
-            while left < right:
-                cur_sum = sorted_nums[left] + sorted_nums[right] + target
-
-                if cur_sum == 0:
-                    ans.append([target, sorted_nums[left], sorted_nums[right]])
-
-                    left += 1
-                    right -= 1
-
-                    while left < right and sorted_nums[left] == sorted_nums[left - 1]:
-                        left += 1
-                    
-                    while left < right and sorted_nums[right] == sorted_nums[right + 1]:
-                        right -= 1
-
-                elif cur_sum < 0:
-                    left += 1
-
+                if total > 0:
+                    k -= 1
+                elif total < 0:
+                    j += 1
                 else:
-                    right -= 1
+                    res.append((nums[i], nums[j], nums[k]))
+                    j += 1
 
-        return ans
+                    while nums[j] == nums[j-1] and j < k:
+                        j += 1
+                    
+        return res
