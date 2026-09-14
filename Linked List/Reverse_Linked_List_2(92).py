db621d7f6c0d43f7de1a67f19e86c5f6b66d2460
Austin Wort <119460845+Runeozzyman@users.noreply.github.com>
2026-09-14 15:48:33 -0600

    Create Reverse_Linked_List_2(92).py
    Check out my posted solution here:
    
    https://leetcode.com/problems/reverse-linked-list-ii/solutions/8521917/beats-100-simple-two-pass-approach-using-u6sa

---
+++ Linked List/Reverse_Linked_List_2(92).py
@@ -0,0 +1,33 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        if not head:
+            return None
+        
+        if left == right:
+            return head
+        
+        stack = []
+        
+        curr = head
+        nodeCounter = 1
+        
+        while curr:
+            if nodeCounter >= left and nodeCounter <= right:
+                stack.append(curr.val)
+            curr = curr.next
+            nodeCounter += 1
+
+        curr = head
+        nodeCounter = 1
+        while curr and stack:
+            if nodeCounter >= left and nodeCounter <= right:
+                curr.val = stack.pop()
+            curr = curr.next
+            nodeCounter += 1
+        
+        return head
